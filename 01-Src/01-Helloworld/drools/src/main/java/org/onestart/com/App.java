e88bd0cd57c76f8147f69a9344e8d37114f397ab
rahgadda <rahgadda@RAHGADDA-IN.in.oracle.com>
2020-04-11 10:41:06 +0530

    Initial Draft

--- 01-Src/01-Helloworld/drools/src/main/java/org/onestart/com/App.java
+++ 01-Src/01-Helloworld/drools/src/main/java/org/onestart/com/App.java
@@ -3,23 +3,34 @@ package org.onestart.com;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class App 
-{
+public class App {
     static final Logger LOG = LoggerFactory.getLogger(App.class);
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
         LOG.info("Creating kieBase");
         KieServices ks = KieServices.get();
         KieContainer kc = ks.getKieClasspathContainer();
 
         LOG.info("Creating kSession");
-        KieSession kSession = kc.newKieSession("ksession-rules");
+        KieSession kSession = kc.newKieSession("hello-world-rules");
 
-        LOG.info("Creating Fact");
-        final HelloWorld helloWorld = new HelloWorld("Rahul");
+        LOG.info("Creating Fact - isGreetingDone = True");
+        HelloWorld helloWorld = new HelloWorld("Rahul");
+        FactHandle factHandel = kSession.insert(helloWorld);
+
+        LOG.info("Running RuleEngine");
+        kSession.fireAllRules();
+
+        LOG.info("Display Updated Message");
+        System.out.println(helloWorld.getMessage());
+
+        LOG.info("Update Fact - isGreetingDone = False");
+        kSession.delete(factHandel);
+        helloWorld.setIsGreetingDone(false);
         kSession.insert(helloWorld);
 
         LOG.info("Running RuleEngine");
